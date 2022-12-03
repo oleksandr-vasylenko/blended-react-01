74632b2f701520a3dc2705be6cf027dcf269cf2d
Ihor Chubov <ihor.chub@gmail.com>
2022-12-03 12:33:40 +0200

    add task 4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,28 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { formatDate } from 'components/helpers';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }) => (
+          <Tr key={id}>
+            <Td>{id}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{formatDate(date)}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
